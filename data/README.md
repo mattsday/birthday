ed17893bee759e710331f4273cf5a6ad331204b8
Matt Day <mattsday@gmail.com>
2022-10-24 10:56:35 +0200

    feat: firestore data import

--- data/README.md
+++ data/README.md
@@ -1,7 +1,31 @@
 # Data Importer
 
-Download products.json from here: https://raw.githubusercontent.com/BestBuyAPIs/open-data-set/master/products.json
+Download `products.json`:
 
-Fix it by replacing any "shipping":"" entries with "shipping":0
+```bash
+wget https://raw.githubusercontent.com/BestBuyAPIs/open-data-set/master/products.json
+```
 
-Configure & run main.go to populate Firestore
+Fix it by replacing any "shipping":"" entries with "shipping":0. You can do this with a simple sed command:
+
+```bash
+sed -i 's/"shipping":""/"shipping":0/g' products.json
+```
+
+Set your Google Cloud project and collection ID:
+```bash
+export GOOGLE_CLOUD_PROJECT=<my_project>
+export FIRESTORE_COLLECTION=products
+```
+
+If not running in Google Cloud, login with the application default credentials:
+
+```bash
+gcloud auth application-default login
+```
+
+Run the import app:
+
+```bash
+go run main.go
+```
---
+++ data/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	firebase "firebase.google.com/go/v4"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+type Product struct {
+	Sku      int     `json:"sku" firestore:"sku"`
+	Name     string  `json:"name" firestore:"name"`
+	Type     string  `json:"type" firestore:"type"`
+	Price    float64 `json:"price" firestore:"price"`
+	Upc      string  `json:"upc" firestore:"upc"`
+	Category []struct {
+		ID   string `json:"id" firestore:"id"`
+		Name string `json:"name" firestore:"name"`
+	} `json:"category" firestore:"category"`
+	Shipping     float64 `json:"shipping" firestore:"shipping"`
+	Description  string  `json:"description" firestore:"description"`
+	Manufacturer string  `json:"manufacturer" firestore:"manufacturer"`
+	Model        string  `json:"model" firestore:"model"`
+	URL          string  `json:"url" firestore:"url"`
+	Image        string  `json:"image" firestore:"image"`
+}
+
+func main() {
+	collection := os.Getenv("FIREBASE_COLLECTION")
+	if collection == "" {
+		log.Fatalln("No Firebase collection specified - do so with the environment variable FIREBASE_COLLECTION")
+	}
+
+	// Open products.json and serialise
+	jsonFile, err := os.Open("products.json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer func(jsonFile *os.File) {
+		err := jsonFile.Close()
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}(jsonFile)
+	byteValue, _ := io.ReadAll(jsonFile)
+	var products []Product
+	err = json.Unmarshal(byteValue, &products)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// Connect to Firestore
+	ctx := context.Background()
+	fb := &firebase.Config{}
+	app, err := firebase.NewApp(ctx, fb)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fs, err := app.Firestore(ctx)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("Inserting %v products to Firestore\n", len(products))
+	for _, product := range products {
+		_, err = fs.Collection(collection).Doc(fmt.Sprintf("%v", product.Sku)).Set(ctx, product)
+		if err != nil {
+			log.Printf("Error inserting %v: %v\n", product.Sku, err)
+		}
+	}
+	log.Println("Done")
+}
